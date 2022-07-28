5dab6b49805fab1729bdf21dcfa12e8627bb93bb
kyuhur2 <kyuhur2@gmail.com>
2022-07-28 16:57:03 +0900

    python two pointers

---
+++ Python/Two_Pointers/3sum.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        for i, element in enumerate(nums):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+            while right > left:
+                val = nums[left] + nums[right] + element
+                if val > 0:
+                    right -= 1
+                elif val < 0:
+                    left += 1
+                else:
+                    result.append([nums[left], nums[right], element])
+                    left += 1
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+
+        return sorted([sorted(i) for i in result])
+
+
+if __name__ == "__main__":
+    input_cases = [[-1, 0, 1, 2, -1, -4], [0, 0, 0]]
+    output_cases = [[[-1, -1, 2], [-1, 0, 1]], [[0, 0, 0]]]
+    solution = Solution()
+    for test_number, element in enumerate(zip(input_cases, output_cases)):
+        input_case, output_case = element
+        if solution.threeSum(input_case) == output_case:
+            print(f"Test {test_number}: Passed.")
+        else:
+            print(f"Test {test_number}: Failed.")
