54c8dd461560ffb99c1b5e7a0857aaa5fd197e7f
kyuhur2 <kyuhur2@gmail.com>
2022-07-29 20:10:29 +0900

    python two pointers

---
+++ Python/Two_Pointers/contain_with_most_water.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxVolume = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            maxVolume = max(
+                maxVolume, min(height[right], height[left]) * (right - left)
+            )
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxVolume
+
+
+if __name__ == "__main__":
+    input_cases = [[1, 8, 6, 2, 5, 4, 8, 3, 7], [1, 1], []]
+    output_cases = [49, 1, 0]
+    solution = Solution()
+    for test_number, element in enumerate(zip(input_cases, output_cases)):
+        input_case, output_case = element
+        if solution.maxArea(input_case) == output_case:
+            print(f"Test {test_number}: Passed.")
+        else:
+            print(f"Test {test_number}: Failed.")
